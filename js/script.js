5d58021d823bf366be01888e8aee6b2c003395ed
Enrico Paolazzi <paolazzienrico@gmail.com>
2021-04-16 16:47:56 +0200

    Milestone 1

--- js/script.js
+++ js/script.js
@@ -112,13 +112,48 @@ const colors = [
 */
 
 // Milestone 1
-// Partendo dalla seguente struttura dati , 
+// Partendo dalla seguente struttura dati, 
 // mostriamo in pagina tutte le icone disponibili come da layout.
+const containerIcons = $('#container-icons');
+printIcons(icons, containerIcons); 
 
 // Milestone 2
 // Coloriamo le icone per tipo
 
 // Milestone 3
 // Creiamo una select con i tipi di icone e usiamola per filtrare le icone
+
+
+/* ------------ FUNCTIONS ------------ */
+
+// Questa funzione stampa le icone in pagina a partire da un array di oggetti
+//
+// objectArray --> l'array di oggetti che rappresenta le icone 
+// container --> l'elemento html sul quale voglio stampare le icone
+//
+// return: void (undefined)
+function printIcons(objectArray, container) {
+	
+	// Devo scorrere l'array con un ciclo foEach e ricavare le informazioni per creare
+	// il template literal
+	objectArray.forEach( (element) => {
+		const iconPrefix = element.prefix;
+		const iconName = element.name;
+		const iconFamily = element.family;
+
+		// Creo template literal
+		const templateIcon = `
+			<div class="single-icon">
+				<i class="${iconFamily} ${iconPrefix}${iconName}"></i>
+				<div class="name">${iconName}</div>
+			</div> 
+		`;
+
+		// Devo stampare il template literal in pagina
+		container.append(templateIcon);
+	});
+
+	
+};
 
 
